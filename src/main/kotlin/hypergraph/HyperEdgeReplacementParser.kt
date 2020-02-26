d1dd6ec0e6a2598fbead33ffeec6598a952fd2a8
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2020-02-26 10:59:06 +0100

    [TRD-512] use typealias

--- src/main/kotlin/hypergraph/HyperEdgeReplacementParser.kt
+++ src/main/kotlin/hypergraph/HyperEdgeReplacementParser.kt
@@ -1,9 +1,11 @@
 package hypergraph
 
 import hypergraph.HyperEdgeReplacementParser.HyperEdge
-import hypergraph.HyperEdgeReplacementParser.HyperGraph
 import hypergraph.HyperEdgeReplacementParser.StateMachine
 import hypergraph.HyperEdgeReplacementParser.heReplace
+import hypergraph.HyperEdgeReplacementParser.hyperGraphOf
+
+typealias HyperGraph<N> = MutableList<HyperEdge<N>>
 
 object HyperEdgeReplacementParser {
 
@@ -13,14 +15,12 @@ object HyperEdgeReplacementParser {
             val target: List<N>
     )
 
-    data class HyperGraph<N>(
-            val edges: MutableList<HyperEdge<N>>
-    )
-
     data class StateMachine<N>(
             val hyperGraph: HyperGraph<N>,
             val rules: Map<String, HyperGraph<N>>
     )
+
+    fun <N> hyperGraphOf(vararg edges: HyperEdge<N>) = mutableListOf(*edges)
 
     internal fun <N> heReplace(stateMachine: StateMachine<N>, label: String) {
         val hyperEdgeToReplace = findHyperEdgeInHyperGraphByLabel(stateMachine.hyperGraph, label)
@@ -30,7 +30,7 @@ object HyperEdgeReplacementParser {
         val iterSourceExternalNodes = hyperEdgeToReplace.source.toList().iterator()
         val iterTargetExternalNodes = hyperEdgeToReplace.target.toList().iterator()
 
-        val copyHyperEdges = hyperGraphToReplaceWith.edges.map { edge ->
+        val copyHyperEdges = hyperGraphToReplaceWith.map { edge ->
             val copySourceNodes = edge.source.map {
                 if (it == "_") iterSourceExternalNodes.next()
                 else it
@@ -43,35 +43,35 @@ object HyperEdgeReplacementParser {
         }
 
         deleteHyperEdgeInHyperGraph(stateMachine.hyperGraph, hyperEdgeToReplace)
-        stateMachine.hyperGraph.edges.addAll(copyHyperEdges)
+        stateMachine.hyperGraph.addAll(copyHyperEdges)
         println("result: ${stateMachine.hyperGraph}")
     }
 
     private fun <N> deleteHyperEdgeInHyperGraph(hyperGraph: HyperGraph<N>, hyperEdgeToReplace: HyperEdge<N>) {
-        val index = hyperGraph.edges.indexOfFirst { it == hyperEdgeToReplace }
-        hyperGraph.edges.removeAt(index)
+        val index = hyperGraph.indexOfFirst { it == hyperEdgeToReplace }
+        hyperGraph.removeAt(index)
     }
 
     private fun <N> findHyperEdgeInHyperGraphByLabel(hyperGraph: HyperGraph<N>, label: String): HyperEdge<N> {
-        return hyperGraph.edges.first { it.label == label }
+        return hyperGraph.first { it.label == label }
     }
 }
 
 fun main() {
     val tokens = listOf("John", "loves", "Mary")
-    val hg = HyperGraph(mutableListOf(HyperEdge("S", listOf("1"), listOf("2"))))
+    val hg = hyperGraphOf(HyperEdge("S", listOf("1"), listOf("2")))
 
     val rules = mapOf(
-            "S" to HyperGraph(mutableListOf(
-                    HyperEdge("JOHN", listOf("_"), listOf("_")))),
-            "JOHN" to HyperGraph(mutableListOf(
+            "S" to hyperGraphOf(
+                    HyperEdge("JOHN", listOf("_"), listOf("_"))),
+            "JOHN" to hyperGraphOf(
                     HyperEdge("John", listOf("_"), listOf("3")),
-                    HyperEdge("LOVES", listOf("3"), listOf("_")))),
-            "LOVES" to HyperGraph(mutableListOf(
+                    HyperEdge("LOVES", listOf("3"), listOf("_"))),
+            "LOVES" to hyperGraphOf(
                     HyperEdge("loves", listOf("_"), listOf("4")),
-                    HyperEdge("MARY", listOf("4"), listOf("_")))),
-            "MARY" to HyperGraph(mutableListOf(
-                    HyperEdge("Mary", listOf("_"), listOf("_"))))
+                    HyperEdge("MARY", listOf("4"), listOf("_"))),
+            "MARY" to hyperGraphOf(
+                    HyperEdge("Mary", listOf("_"), listOf("_")))
     )
 
     val stateMachine = StateMachine(hg, rules)
