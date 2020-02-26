6d26140249c5dd527fde004604abfa9a7dca8197
Bram Buitendijk <bram.buitendijk@di.huc.knaw.nl>
2020-02-26 11:13:16 +0100

    [TRD-512] remove object wrapper

--- src/main/kotlin/hypergraph/HyperEdgeReplacementParser.kt
+++ src/main/kotlin/hypergraph/HyperEdgeReplacementParser.kt
@@ -1,60 +1,52 @@
 package hypergraph
 
-import hypergraph.HyperEdgeReplacementParser.HyperEdge
-import hypergraph.HyperEdgeReplacementParser.StateMachine
-import hypergraph.HyperEdgeReplacementParser.heReplace
-import hypergraph.HyperEdgeReplacementParser.hyperGraphOf
-
 typealias HyperGraph<N> = MutableList<HyperEdge<N>>
 
-object HyperEdgeReplacementParser {
+fun <N> hyperGraphOf(vararg edges: HyperEdge<N>) = mutableListOf(*edges)
 
-    data class HyperEdge<N>(
-            val label: String,
-            val source: List<N>,
-            val target: List<N>
-    )
+data class HyperEdge<N>(
+        val label: String,
+        val source: List<N>,
+        val target: List<N>
+)
 
-    data class StateMachine<N>(
-            val hyperGraph: HyperGraph<N>,
-            val rules: Map<String, HyperGraph<N>>
-    )
+data class StateMachine<N>(
+        val hyperGraph: HyperGraph<N>,
+        val rules: Map<String, HyperGraph<N>>
+)
 
-    fun <N> hyperGraphOf(vararg edges: HyperEdge<N>) = mutableListOf(*edges)
+internal fun <N> heReplace(stateMachine: StateMachine<N>, label: String) {
+    val hyperEdgeToReplace = findHyperEdgeInHyperGraphByLabel(stateMachine.hyperGraph, label)
 
-    internal fun <N> heReplace(stateMachine: StateMachine<N>, label: String) {
-        val hyperEdgeToReplace = findHyperEdgeInHyperGraphByLabel(stateMachine.hyperGraph, label)
+    val hyperGraphToReplaceWith = stateMachine.rules[label] ?: error("No applicable rule found")
 
-        val hyperGraphToReplaceWith = stateMachine.rules[label] ?: error("No applicable rule found")
+    val iterSourceExternalNodes = hyperEdgeToReplace.source.toList().iterator()
+    val iterTargetExternalNodes = hyperEdgeToReplace.target.toList().iterator()
 
-        val iterSourceExternalNodes = hyperEdgeToReplace.source.toList().iterator()
-        val iterTargetExternalNodes = hyperEdgeToReplace.target.toList().iterator()
-
-        val copyHyperEdges = hyperGraphToReplaceWith.map { edge ->
-            val copySourceNodes = edge.source.map {
-                if (it == "_") iterSourceExternalNodes.next()
-                else it
-            }.toList()
-            val copyTargetNodes = edge.target.map {
-                if (it == "_") iterTargetExternalNodes.next()
-                else it
-            }.toList()
-            HyperEdge(edge.label, copySourceNodes, copyTargetNodes)
-        }
-
-        deleteHyperEdgeInHyperGraph(stateMachine.hyperGraph, hyperEdgeToReplace)
-        stateMachine.hyperGraph.addAll(copyHyperEdges)
-        println("result: ${stateMachine.hyperGraph}")
+    val copyHyperEdges = hyperGraphToReplaceWith.map { edge ->
+        val copySourceNodes = edge.source.map {
+            if (it == "_") iterSourceExternalNodes.next()
+            else it
+        }.toList()
+        val copyTargetNodes = edge.target.map {
+            if (it == "_") iterTargetExternalNodes.next()
+            else it
+        }.toList()
+        HyperEdge(edge.label, copySourceNodes, copyTargetNodes)
     }
 
-    private fun <N> deleteHyperEdgeInHyperGraph(hyperGraph: HyperGraph<N>, hyperEdgeToReplace: HyperEdge<N>) {
-        val index = hyperGraph.indexOfFirst { it == hyperEdgeToReplace }
-        hyperGraph.removeAt(index)
-    }
+    deleteHyperEdgeInHyperGraph(stateMachine.hyperGraph, hyperEdgeToReplace)
+    stateMachine.hyperGraph.addAll(copyHyperEdges)
+    println("result: ${stateMachine.hyperGraph}")
+}
 
-    private fun <N> findHyperEdgeInHyperGraphByLabel(hyperGraph: HyperGraph<N>, label: String): HyperEdge<N> {
-        return hyperGraph.first { it.label == label }
-    }
+private fun <N> deleteHyperEdgeInHyperGraph(hyperGraph: HyperGraph<N>, hyperEdgeToReplace: HyperEdge<N>) {
+    val index = hyperGraph.indexOfFirst { it == hyperEdgeToReplace }
+    hyperGraph.removeAt(index)
+}
+
+private fun <N> findHyperEdgeInHyperGraphByLabel(hyperGraph: HyperGraph<N>, label: String): HyperEdge<N> {
+    return hyperGraph.first { it.label == label }
 }
 
 fun main() {
